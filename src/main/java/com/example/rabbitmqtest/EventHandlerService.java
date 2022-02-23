027a704826e7ea7e45e209da5f59c472f21d51f7
Biela <Mateusz.Biela@com2m.de>
2022-02-23 15:59:32 +0100

    sending message after start

--- src/main/java/com/example/rabbitmqtest/EventHandlerService.java
+++ src/main/java/com/example/rabbitmqtest/EventHandlerService.java
@@ -5,7 +5,6 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 
-
 @Service
 public class EventHandlerService {
 
--- src/main/java/com/example/rabbitmqtest/RabbitmqTestApplication.java
+++ src/main/java/com/example/rabbitmqtest/RabbitmqTestApplication.java
@@ -1,9 +1,14 @@
 package com.example.rabbitmqtest;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.event.EventListener;
 
 @ComponentScan(value = {"com.example.rabbitmqtest"}, excludeFilters = {@ComponentScan.Filter(
 		type = FilterType.ASSIGNABLE_TYPE,
@@ -13,8 +18,17 @@ import org.springframework.context.annotation.FilterType;
 @SpringBootApplication
 public class RabbitmqTestApplication {
 
+	@Autowired
+	private RabbitTemplate template;
+
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqTestApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void doSomethingAfterStartup() {
+		String message = "{\"itemId\":\"123\", \"payload\":\""+ StringUtils.repeat("*", 2000000) + "\"}";
+		this.template.convertAndSend("events.exchange", "private.topic.test-service", message);
 	}
 
 }
